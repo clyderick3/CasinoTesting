71027d6939a3d795bd543cede34b3d05ebc5a3c5
Clyde Broderick <clyderick3@gmail.com>
2022-03-23 12:00:19 -0400

    Properly displayed my slot return.

--- SlotsCasino.java
+++ SlotsCasino.java
@@ -11,27 +11,16 @@ public class SlotsCasino {
 
         System.out.println("Welcome to the Slots game!\nAre you feeling lucky?");
         String[] Winners = {"Black Panther", "Iron Man", "Captain America","The Hulk","Thor","Spider-man","Thanos"};
-        column1 = random.nextInt(Winners.length -1);
+        column1 = random.nextInt(Winners.length-1);
         column2 = random.nextInt(Winners.length-1);
         column3 = random.nextInt(Winners.length-1);
 
-        System.out.println("|" + column1 + "|" + column2 + "|" + column3 + "|");
+        System.out.println("|" + Winners[column1] + "|" + Winners[column2] + "|" + Winners[column3] + "|");
 
     }
 
     public void play(){
-        Random random = new Random();
 
-        int column1;
-        int column2;
-        int column3;
-
-        System.out.println("Welcome to the Slots game!\nAre you feeling lucky?");
-        String[] Options = {"Black Panther", "Iron Man", "Captain America","The Hulk","Thor","Spider-man","Thanos"};
-        column1 = random.nextInt(Options.length -1);
-        column2 = random.nextInt(Options.length-1);
-        column3 = random.nextInt(Options.length-1);
-        System.out.println(column1 + column2 +  column3);
     }
 
 }
